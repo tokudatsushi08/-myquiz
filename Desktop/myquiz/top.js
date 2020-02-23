904e03e4e6f33c3872098e457dbd1bda85539689
tokudatsushi08 <atsushi.t08@gmail.com>
2020-02-23 14:00:42 +0900

    クイズを取得する

--- Desktop/myquiz/top.js
+++ Desktop/myquiz/top.js
@@ -1,6 +1,25 @@
 'user strict'
 
 {
+    const startBtn = document.getElementById('startBtn');
+    const quizs = [];
+    startBtn.addEventListener('click', function () {
 
-
+        // fetch→URLからAPIを持ってくる処理
+        fetch('https://opentdb.com/api.php?amount=10')
+            .then(function (response) {
+                if (response.ok) {
+                    return response.text();
+                } else {
+                    return Promise.reject(new Error('エラーです'))
+                };
+            })
+            .then(function (response) {
+                const objRes = JSON.parse(response);
+                console.log(objRes);
+                const quizContent = objRes.results;
+                quizs.push(quizContent);
+                console.log(quizs);
+            })
+    });
 }
