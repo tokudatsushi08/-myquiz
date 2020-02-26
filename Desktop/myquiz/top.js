547107e538a2e931252ae21bbbee1afea49efece
tokudatsushi08 <atsushi.t08@gmail.com>
2020-02-26 23:21:36 +0900

    再修正

--- Desktop/myquiz/top.js
+++ Desktop/myquiz/top.js
@@ -30,38 +30,4 @@
             })
     })
 
-    const showQuiz = function () {
-        const quizCategory = quizs[0].category;
-        const quizDifficulty = quizs[0].difficulty;
-        const quizQuestion = quizs[0].question;
-        console.log(quizQuestion);
-
-        questionNumber.innerHTML = 1;
-        questionGenre.innerHTML = quizCategory;
-        questionDifficulty.innerHTML = quizDifficulty;
-        question.innerHTML = quizQuestion;
-
-        const answerBtn_01 = document.createElement('button');
-        const answerBtn_02 = document.createElement('button');
-        const answerBtn_03 = document.createElement('button');
-        const answerBtn_04 = document.createElement('button');
-
-        topDisplay.appendChild(answerBtn_01);
-        topDisplay.appendChild(answerBtn_02);
-        topDisplay.appendChild(answerBtn_03);
-        topDisplay.appendChild(answerBtn_04);
-
-        topBtn.removeChild(startBtn);
-
-        // remove(startBtn);
-
-        answerBtn_01.innerHTML = quizs[0].correct_answer;
-        answerBtn_02.innerHTML = quizs[0].incorrect_answers[0];
-        answerBtn_03.innerHTML = quizs[0].incorrect_answers[1];
-        answerBtn_04.innerHTML = quizs[0].incorrect_answers[2];
-
-
-
-    };
-
 }
