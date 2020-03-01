d84581389712f77c1504caa19262bb552ad5f531
tokudatsushi08 <atsushi.t08@gmail.com>
2020-03-01 13:08:33 +0900

    最初のクイズを表示

--- Desktop/myquiz/top.js
+++ Desktop/myquiz/top.js
@@ -22,25 +22,49 @@
             // Promiseオブジェクトを使う事で、コールバック関数（ある動きをした後に特定の関数を行うもの）が実行される事はない。
             // Promiseオブジェクトを使い、成功するとResponseオブジェクトを返してくれる。
             .then(function (response) {
+                console.log(response);
                 return response.json();
             })
-            .then(function (data) {
-                console.log(data);
-                console.log(data.results);
+            .then(function (json) {
+                console.log(json);
+                console.log(json.results[0]);
+                for (let i = 0; i < 10; i++) {
+                    quizs.push(json.results[i]);
+                }
+                // 最初に表示をさせる処理
+                questionNumber.innerHTML = 1;
+                questionGenre.innerHTML = quizs[0].category;
+                questionDifficulty.innerHTML = quizs[0].difficulty;
+                question.innerHTML = quizs[0].question;
+                topBtn.removeChild(startBtn);
+
+                console.log(quizs[0].incorrect_answers.length);
+
+                if (quizs[0].incorrect_answers.length === 1) {
+                    const answerBtn = document.createElement('button');
+                    console.log(answerBtn);
+                    answerBtn.textContent = quizs[0].incorrect_answers;
+                    topBtn.appendChild(answerBtn);
+                } else {
+                    const firstBtn = document.createElement('button');
+                    const secondeBtn = document.createElement('button');
+                    const thirdBtn = document.createElement('button');
+                    firstBtn.textContent = quizs[0].incorrect_answers[0];
+                    secondeBtn.textContent = quizs[0].incorrect_answers[1];
+                    thirdBtn.textContent = quizs[0].incorrect_answers[2];
+                    topBtn.appendChild(firstBtn);
+                    topBtn.appendChild(secondeBtn);
+                    topBtn.appendChild(thirdBtn);
+                };
+
+                const correctBtn = document.createElement('button');
+                correctBtn.textContent = quizs[0].correct_answer;
+                topBtn.appendChild(correctBtn);
+
             })
             .catch(function (error) {
                 return error;
             })
 
-
-    }
-
-    // 画面に表示される処理
-    const showQuiz = function () {
-        console.log(quizs[0]);
-        questionNumber.innerHTML = 1;
-        questionGenre.innerHTML = quizs[0].category;
-        questionDifficulty.innerHTML = quizs[0].difficulty;
-        question.innerHTML = quizs[0].question;
-    }
+    };
 }
