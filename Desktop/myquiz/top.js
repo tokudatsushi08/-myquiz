f6ca207598bcb63ad296405677bf5e701177ffcb
徳田篤司 <atsushi@tokudaatsushinoMacBook-puro.local>
2020-03-20 18:33:12 +0900

    最初のdevelop

--- Desktop/myquiz/top.js
+++ Desktop/myquiz/top.js
@@ -1,49 +1,9 @@
 'user strict'
 
 {
-    const startBtn = document.getElementById('start-btn');
-    const topBtn = document.getElementById('top-btn');
-    let quizs = [];
-    const questionNumber = document.getElementById('top-section');
-    const question = document.getElementById('question');
-    startBtn.addEventListener('click', function () {
 
-        questionNumber.innerHTML = "取得中";
-        question.innerHTML = "少々お待ちください";
-        getQuiz();
-    })
 
-    // クイズを取ってくる処理
-    const getQuiz = function () {
-        // fetch→URLからAPIを持ってくる処理
 
-        fetch('http://opentdb.com/api.php?amount=10')
-            // fetchで外部APIを取得する(リクエスト)。fetchを使う事で、Promiseオブジェクトを返してくれる（レスポンス）。
-            // promiseオブジェクトとは、非同期処理の成功・失敗を表してくれるもので非同期処理に最適。
-            // Promiseオブジェクトを使う事で、コールバック関数（ある動きをした後に特定の関数を行うもの）が実行される事はない。
-            // Promiseオブジェクトを使いレスポンスが正しい回答で回答あれば、thenの処理をし、誤った回答であればcatchの処理を行う。
-            // 成功した場合は、responseの内容を引数に入れられる。
-            .then(function (response) {
 
-                console.log(response);
-                return response.json();
-            })
-            // このthenの引数には、上記で取得したresponseをjson型にした物を引数に入れられる。
-            .then(function (json) {
-                console.log(json);
-                console.log(json.results[0]);
-                // jsonの中のresultsという配列を取得する＝slice
-                const quizResults = json.results.slice(0);
-                console.log(quizResults);
-                // quizsの配列の中にquizseResultを入れる
-                quizs = quizResults.slice(0, quizResults.length);
-                console.log(quizs);
-
-                topBtn.removeChild(startBtn);
-            })
-            .catch(function (error) {
-                return error;
-            })
-    };
 }
 
